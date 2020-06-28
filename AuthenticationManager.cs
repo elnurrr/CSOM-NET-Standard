fd5ddde489f69a5d25a2c99ae56ff536e0914cf6
Babayev Elnur (2) <elnur.babayev.2@volvo.com>
2020-06-28 11:02:43 +0200

    initial commit

---
+++ AuthenticationManager.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Concurrent;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_CSOM_NETStandard
+{
+    public class AuthenticationManager : IDisposable
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+        private const string tenantName = "contoso.onmicrosoft.com";
+
+        // Token cache handling
+        private static readonly SemaphoreSlim semaphoreSlimTokens = new SemaphoreSlim(1);
+        private AutoResetEvent tokenResetEvent = null;
+        private readonly ConcurrentDictionary<string, string> tokenCache = new ConcurrentDictionary<string, string>();
+        private bool disposedValue;
+
+        internal class TokenWaitInfo
+        {
+            public RegisteredWaitHandle Handle = null;
+        }
+
+        public ClientContext GetContext(Uri web, string clientId, string clientSecret)
+        {
+            ClientContext context = new ClientContext(web);
+            context.ExecutingWebRequest += (sender, e) =>
+            {
+                string accessToken = EnsureAccessTokenAsync(web, clientId, clientSecret).GetAwaiter().GetResult();
+                e.WebRequestExecutor.RequestHeaders["Authorization"] = "Bearer " + accessToken;
+            };
+
+            return context;
+        }
+
+        public async Task<string> EnsureAccessTokenAsync(Uri web, string clientId, string clientSecret)
+        {
+            string accessTokenFromCache = TokenFromCache(web, tokenCache);
+            if (accessTokenFromCache == null)
+            {
+                await semaphoreSlimTokens.WaitAsync().ConfigureAwait(false);
+                try
+                {
+                    // No async methods are allowed in a lock section
+                    string accessToken = await AcquireTokenAsync(web, clientId, clientSecret).ConfigureAwait(false);
+                    AddTokenToCache(web, tokenCache, accessToken);
+
+                    // Register a thread to invalidate the access token once's it's expired
+                    tokenResetEvent = new AutoResetEvent(false);
+                    TokenWaitInfo wi = new TokenWaitInfo();
+                    wi.Handle = ThreadPool.RegisterWaitForSingleObject(
+                        tokenResetEvent,
+                        async (state, timedOut) =>
+                        {
+                            if (!timedOut)
+                            {
+                                TokenWaitInfo wi = (TokenWaitInfo)state;
+                                if (wi.Handle != null)
+                                {
+                                    wi.Handle.Unregister(null);
+                                }
+                            }
+                            else
+                            {
+                                try
+                                {
+                                    // Take a lock to ensure no other threads are updating the SharePoint Access token at this time
+                                    await semaphoreSlimTokens.WaitAsync().ConfigureAwait(false);
+                                    RemoveTokenFromCache(web, tokenCache);
+                                    Console.WriteLine($"Cached token for resource {web.DnsSafeHost} and clientId {clientId} expired");
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine($"Something went wrong during cache token invalidation: {ex.Message}");
+                                    RemoveTokenFromCache(web, tokenCache);
+                                }
+                                finally
+                                {
+                                    semaphoreSlimTokens.Release();
+                                }
+                            }
+                        },
+                        wi,
+                        (uint)CalculateThreadSleep(accessToken).TotalMilliseconds,
+                        true
+                    );
+
+                    return accessToken;
+                }
+                finally
+                {
+                    semaphoreSlimTokens.Release();
+                }
+            }
+            else
+                return accessTokenFromCache;
+        }
+
+        private async Task<string> AcquireTokenAsync(Uri web, string clientId, string clientSecret)
+        {
+            var body = "grant_type=client_credentials" +
+                $"&resource=00000003-0000-0ff1-ce00-000000000000/{web.DnsSafeHost}@{tenantName}" +
+                $"&client_id={clientId}@{tenantName}" +
+                $"&client_secret={clientSecret}";
+
+            using (var stringContent = new StringContent(body, Encoding.UTF8, "application/x-www-form-urlencoded"))
+            {
+                var result = await httpClient.PostAsync($"https://accounts.accesscontrol.windows.net/{tenantName}/tokens/OAuth/2", stringContent)
+                    .ContinueWith((response) =>
+                    {
+                        return response.Result.Content.ReadAsStringAsync().Result;
+                    })
+                    .ConfigureAwait(false);
+
+                var tokenResult = JsonSerializer.Deserialize<JsonElement>(result);
+                var token = tokenResult.GetProperty("access_token").GetString();
+                return token;
+            }
+        }
+
+        private static string TokenFromCache(Uri web, ConcurrentDictionary<string, string> tokenCache)
+        {
+            if (tokenCache.TryGetValue(web.DnsSafeHost, out string accessToken))
+                return accessToken;
+
+            return null;
+        }
+
+        private static void AddTokenToCache(Uri web, ConcurrentDictionary<string, string> tokenCache, string newAccessToken)
+        {
+            if (tokenCache.TryGetValue(web.DnsSafeHost, out string currentAccessToken))
+                tokenCache.TryUpdate(web.DnsSafeHost, newAccessToken, currentAccessToken);
+            else
+                tokenCache.TryAdd(web.DnsSafeHost, newAccessToken);
+        }
+
+        private static void RemoveTokenFromCache(Uri web, ConcurrentDictionary<string, string> tokenCache)
+        {
+            tokenCache.TryRemove(web.DnsSafeHost, out string currentAccessToken);
+        }
+
+        private static TimeSpan CalculateThreadSleep(string accessToken)
+        {
+            var token = new System.IdentityModel.Tokens.Jwt.JwtSecurityToken(accessToken);
+            var lease = GetAccessTokenLease(token.ValidTo);
+            lease = TimeSpan.FromSeconds(lease.TotalSeconds - TimeSpan.FromMinutes(5).TotalSeconds > 0 ? lease.TotalSeconds - TimeSpan.FromMinutes(5).TotalSeconds : lease.TotalSeconds);
+            return lease;
+        }
+
+        private static TimeSpan GetAccessTokenLease(DateTime expiresOn)
+        {
+            DateTime now = DateTime.UtcNow;
+            DateTime expires = expiresOn.Kind == DateTimeKind.Utc ? expiresOn : TimeZoneInfo.ConvertTimeToUtc(expiresOn);
+            TimeSpan lease = expires - now;
+            return lease;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if (tokenResetEvent != null)
+                    {
+                        tokenResetEvent.Set();
+                        tokenResetEvent.Dispose();
+                    }
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_CSOM_NETStandard
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Uri site = new Uri("https://contoso.sharepoint.com/Sites/dev01");
+            string clientId = "ae777e7c-a1e0-45a4-a15a-cf53d6c3152f";
+            string clientSecret = "AJh0C8cJd4-K83JCDK***";
+
+            // Note: The PnP Sites Core AuthenticationManager class also supports this
+            using (var authenticationManager = new AuthenticationManager())
+            {
+                using (var context = authenticationManager.GetContext(site, clientId, clientSecret))
+                {
+                    context.Load(context.Web, p => p.Title);
+                    await context.ExecuteQueryAsync();
+                    Console.WriteLine($"Title: {context.Web.Title}");
+
+                    var list = context.Web.Lists.GetByTitle("Links");
+                    ListItemCreationInformation itemCreateInfo = new ListItemCreationInformation();
+                    ListItem newItem = list.AddItem(itemCreateInfo);
+                    newItem["Title"] = "My New Item!";
+                    newItem.Update();
+                    await context.ExecuteQueryAsync();
+                    Console.WriteLine(newItem.Id);
+                }
+            }
+        }
+    }
+}
